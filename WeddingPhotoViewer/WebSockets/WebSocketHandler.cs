470d70c2b12ce9f47e5822b7a17c1964b2b04fa9
DESKTOP-TAFLEEF\anagura <anagura@gmail.com>
2018-02-07 00:31:41 +0900

    websocketサーバから送られた画像を表示できるように

--- WeddingPhotoViewer/WebSockets/WebSocketHandler.cs
+++ WeddingPhotoViewer/WebSockets/WebSocketHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Net.WebSockets;
 using System.Threading;
@@ -13,31 +14,29 @@ namespace WeddingPhotoViewer
 
         public async Task Photo(HttpContext context, WebSocket webSocket)
         {
-            if (context.Request.Headers.TryGetValue("X-PhotoViewerId", out var id))
-            {
-                browsers[id] = webSocket;
-                var cancellToken = new CancellationTokenSource(TimeSpan.FromMinutes(1));
+            var id = context.TraceIdentifier;
+            browsers[id] = webSocket;
+            var cancellToken = new CancellationTokenSource(TimeSpan.FromMinutes(1));
 
-                try
+            try
+            {
+                var buffer = new byte[1024 * 4];
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellToken.Token);
+                while (!result.CloseStatus.HasValue)
                 {
-                    var buffer = new byte[1024 * 4];
-                    var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellToken.Token);
-                    while (!result.CloseStatus.HasValue)
-                    {
-                        // browser keep alive message
-                        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellToken.Token);
-                    }
-                    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, cancellToken.Token);
-                    browsers.Remove(id);
+                    // browser keep alive message
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellToken.Token);
                 }
-                catch (TaskCanceledException)
-                {
-                    browsers.Remove(id);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
+                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, cancellToken.Token);
+                browsers.Remove(id);
+            }
+            catch (TaskCanceledException)
+            {
+                browsers.Remove(id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
             }
         }
 
@@ -47,19 +46,34 @@ namespace WeddingPhotoViewer
             var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             while (!result.CloseStatus.HasValue)
             {
+                List<string> deleteList = new List<string>();
                 foreach (var browser in browsers)
                 {
                     try
                     {
-                        // send image url
-                        await browser.Value.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                        if (browser.Value.State == WebSocketState.Open)
+                        {
+                            // send image url
+                            await browser.Value.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                        } else
+                        {
+                            deleteList.Add(browser.Key);
+                        }
                     }
-                    catch
+                    catch (Exception ex)
                     {
                         // ignore
+                        Console.WriteLine(ex);
+                        deleteList.Add(browser.Key);
                     }
                 }
-
+                if (deleteList.Any())
+                {
+                    deleteList.ForEach(x =>
+                   {
+                       browsers.Remove(x);
+                   });
+                }
                 result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             }
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
