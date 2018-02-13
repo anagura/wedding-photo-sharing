64e83af5887f480b98e130e726c5ec1ba41d6396
Otsuka Hiroshi <anagura@gmail.com>
2018-02-14 01:08:37 +0900

    ブラウザ接続時に貯めてた分をまとめて送る

--- WeddingPhotoViewer/WebSockets/WebSocketHandler.cs
+++ WeddingPhotoViewer/WebSockets/WebSocketHandler.cs
@@ -11,6 +11,7 @@ namespace WeddingPhotoViewer
     public class WebSocketHandler
     {
         private Dictionary<string, WebSocket> browsers = new Dictionary<string, WebSocket>();
+        private List<WebSocketMessage> messageList = new List<WebSocketMessage>();
 
         public async Task Photo(HttpContext context, WebSocket webSocket)
         {
@@ -19,6 +20,12 @@ namespace WeddingPhotoViewer
 
             try
             {
+                // send queued message
+                foreach (var msg in messageList)
+                {
+                    await webSocket.SendAsync(msg.Message, msg.MessageType, msg.EndOfMessage, CancellationToken.None);
+                }
+
                 var buffer = new byte[1024 * 4];
                 var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
                 while (!result.CloseStatus.HasValue)
@@ -45,6 +52,18 @@ namespace WeddingPhotoViewer
             var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             while (!result.CloseStatus.HasValue)
             {
+                // 内部キューに保存
+                var sendMessage = new ArraySegment<byte>(buffer, 0, result.Count);
+                messageList.Add(new WebSocketMessage(){
+                    EndOfMessage = result.EndOfMessage,
+                    MessageType = result.MessageType,
+                    Message = sendMessage
+                });
+
+                if (messageList.Count() > 10) {
+                    messageList.Remove(messageList.FirstOrDefault());
+                }
+
                 List<string> deleteList = new List<string>();
                 foreach (var browser in browsers)
                 {
@@ -53,7 +72,7 @@ namespace WeddingPhotoViewer
                         if (browser.Value.State == WebSocketState.Open)
                         {
                             // send image url
-                            await browser.Value.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                            await browser.Value.SendAsync(sendMessage, result.MessageType, result.EndOfMessage, CancellationToken.None);
                         } else
                         {
                             deleteList.Add(browser.Key);
@@ -77,5 +96,11 @@ namespace WeddingPhotoViewer
             }
             await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
+    }
+
+    public class WebSocketMessage {
+        public ArraySegment<byte> Message { get; set; }
+        public bool EndOfMessage { get; set; }
+        public WebSocketMessageType MessageType { get; set; }
     }
 }
